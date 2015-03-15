cf328178c7c39475b1cb0a8f6b0dc522977a2b5d
Hannes Mehnert <hannes@mehnert.org>
2015-03-15 19:01:37 +0000

    instead of first check the given ?host, search for given fingerprint and validate the hostname. this enables fingerprint (client verification) for servers: a client certificate is presented, but the server does not know upfront which client is talking to it (at least that's how our current setup is -- config.peer_name of a server config is always None).

--- lib/certificate.ml
+++ lib/certificate.ml
@@ -444,29 +444,28 @@ let valid_cas ?time cas =
     (fun cert -> is_success @@ is_ca_cert_valid time cert)
     cas
 
-let trust_fingerprint : ?host:host -> ?time:float -> hash:Hash.hash -> fingerprints:(string * Cstruct.t) list -> certificate list -> [ `Ok of certificate option | `Fail of certificate_failure ] =
+let trust_fingerprint : ?host:host -> ?time:float -> hash:Hash.hash ->
+  fingerprints:(string * Cstruct.t) list -> certificate list ->
+  [ `Ok of certificate option | `Fail of certificate_failure ] =
   fun ?host ?time ~hash ~fingerprints ->
   function
   | [] -> `Fail NoCertificate
   | server::_ ->
-    let verify_fingerprint name fingerprint fingerprints =
-      (try Ok (List.find (fun (n, _) -> n = name) fingerprints)
-       with Not_found -> fail ServerNameNotPresent) >>= fun (_, fp) ->
-      match fp, fingerprint with
-      | f, f' when Cs.equal f f' -> Ok None
-      | _ -> fail (InvalidFingerprint server)
-    and cert_fp = Hash.digest hash server.raw
+    let verify_fingerprint server fingerprints =
+      let cert_fp = Hash.digest hash server.raw in
+      (try Ok (List.find (fun (_, fp) -> Cs.equal fp cert_fp) fingerprints)
+       with Not_found -> fail (InvalidFingerprint server)) >>= fun (name, _) ->
+      if maybe_validate_hostname server (Some (`Wildcard name)) then
+        Ok None
+      else
+        fail ServerNameNotPresent
     in
 
     let res =
-      match host with
-      | None -> fail NoServerName
-      | Some (`Wildcard name)
-      | Some (`Strict name) ->
-        match validate_time time server, maybe_validate_hostname server host with
-        | true , true  -> verify_fingerprint name cert_fp fingerprints
-        | false, _     -> fail (CertificateExpired server)
-        | _    , false -> fail (InvalidServerName server)
+      match validate_time time server, maybe_validate_hostname server host with
+      | true , true  -> verify_fingerprint server fingerprints
+      | false, _     -> fail (CertificateExpired server)
+      | _    , false -> fail (InvalidServerName server)
     in
     lower res
 
